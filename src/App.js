30ce276316a992b57ea68b0721fb36d1bdfc0431
Md Rakibur Rahman <mdrakibur179@gmail.com>
2024-08-28 23:03:30 +0600

    used https instead of http

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
           );
 
           if (!res.ok)
@@ -281,7 +281,7 @@ function MovieDescription({
         try {
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
 
           const data = await res.json();
