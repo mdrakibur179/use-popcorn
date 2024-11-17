2a53113facba814f5db918400cbb4f199ad6dbd9
Md Rakibur Rahman <mdrakibur179@gmail.com>
2024-11-17 20:15:28 +0600

    url fixed

--- src/App.js
+++ src/App.js
@@ -243,7 +243,7 @@ function MovieDescription({
         try {
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
 
           const data = await res.json();
